846901db7741ec05a0de0aa5d22d871098c9c36a
prajayshah <prajay.shah@mail.utoronto.ca>
2023-01-25 21:43:57 -0500

    Initial commit

---
+++ gpt3.py
@@ -0,0 +1,30 @@
+import openai
+from _utils import apikey
+
+## Call the API key under your account (in a secure way)
+openai.api_key = apikey
+
+
+def GPT_Completion(texts):
+    response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt = texts,
+    temperature = 0.6,
+    top_p = 1,
+    max_tokens = 500,
+    frequency_penalty = 0,
+    presence_penalty = 0
+    )
+    return print(response.choices[0].text)
+
+a = True
+
+while a:
+    print('\n')
+    query = str(input('Query GPT-3:'))
+    GPT_Completion(query)
+    print('\n')
+
+
+
+
